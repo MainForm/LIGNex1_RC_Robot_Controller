f6b2d181103081eaaeada61eb15a9505672c3a63
sangMin <tkdalsdl1013@naver.com>
2026-03-07 23:52:49 +0900

    폴더, 파일 구조 생성

---
+++ App/Controller/Common/UGV_Packet.hpp
@@ -0,0 +1,13 @@
+// 통신 구조체 패딩(Padding) 방지 선언 (매우 중요!)
+#pragma pack(push, 1) 
+
+typedef struct Data_Packet {
+    uint16_t x_data;  // 2048 기준 좌우 (12-bit ADC 원본: 0 ~ 4095)
+    uint16_t y_data;  // 2048 기준 앞뒤 (12-bit ADC 원본: 0 ~ 4095)
+    uint16_t bot_ser; // 하단(Base) 서보 가변저항 값
+    uint16_t mid_ser; // 중단(Inner) 서보 가변저항 값
+    uint16_t top_ser; // 상단(Outer) 서보 가변저항 값
+    uint8_t  flags;   // 상태 플래그 (모드 정보, 집게 버튼 상태 등)
+} Data;
+
+#pragma pack(pop)
--- App/ISR/Inc/Interrupts.hpp
+++
@@ -1,4 +0,0 @@
-#ifndef __ISR_H__
-#define __ISR_H__
-
-#endif // __ISR_H__
